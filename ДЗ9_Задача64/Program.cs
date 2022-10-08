57b9c428bf9472bbbdfdfa355f01105dc89afb94
Sergey Tyumentsev <samposebe@ngs.ru>
2022-10-09 02:37:33 +0300

    :Задача 64

---
+++ ДЗ9_Задача64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Задайте значение числа N: ");
+int number = int.Parse(Console.ReadLine());
+int numbercurrent = number - 1;
+
+if (number < 1) Console.Write($"Число {number} не является натуральным.");
+if (number == 1) Console.Write($"N={number} -> \"{number}\"");
+if (number > 1)
+{
+    void PrintReqursive(int numbercurrent)
+    {
+        Console.Write($", ");
+        Console.Write(numbercurrent);
+        numbercurrent--; //numbercurrent = numbercurrent - 1;
+        if (numbercurrent > 0) PrintReqursive(numbercurrent);
+    }
+    Console.Write($"N={number} -> \"{number}");
+    PrintReqursive(numbercurrent);
+    Console.Write($"\"");
+}
