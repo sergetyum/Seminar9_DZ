d70c425268ef75c00989e4f485d732198d7b7dee
Sergey Tyumentsev <samposebe@ngs.ru>
2022-10-10 13:59:53 +0300

    :Задача 66

---
+++ ДЗ9_Задача66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Задайте значение числа M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Задайте значение числа N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1) Console.Write("Оба числа должны быть натуральными.");
+if (numberM == numberN) Console.Write("Числа одинаковые.");
+
+int summaOfNaturalElements = 0;
+
+static int SummaOfNaturalElementsRecursive(int numberM, int numberN, int summaOfNaturalElements)
+{
+    summaOfNaturalElements = summaOfNaturalElements + numberM;
+    numberM++;
+    if (numberM <= numberN) SummaOfNaturalElementsRecursive(numberM, numberN, summaOfNaturalElements);
+    else
+    {
+        Console.Write($"Сумма натуральных чисел в промежутке от M до N равна: {summaOfNaturalElements}");
+    }
+    return summaOfNaturalElements;
+}
+SummaOfNaturalElementsRecursive(numberM, numberN, summaOfNaturalElements);
